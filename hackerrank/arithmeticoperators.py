fd51aa2d1014278b5c8b24adb6f50cf30a7cc501
John <write2johnjoseph@gmail.com>
2022-01-19 17:46:40 +0530

    Update arithmeticoperators.py

--- hackerrank/arithmeticoperators.py
+++ hackerrank/arithmeticoperators.py
@@ -11,34 +11,47 @@
 # b=5
 
 # Print the following:
-
 # 8
 # -2
 # 15
-# Input Format
 
-# The first line contains the first integer, .
-# The second line contains the second integer, .
+# Input Format:
+# The first line contains the first integer, a.
+# The second line contains the second integer, b.
 
-# Constraints
+# Constraints:
+# 1<=a<=10^10
+# 1<=b<=10^10
 
-
-
-# Output Format
-
+# Output Format:
 # Print the three lines as explained above.
 
-# Sample Input 0
-
+# Sample Input 0:
 # 3
 # 2
-# Sample Output 0
 
+# Sample Output 0:
 # 5
 # 1
 # 6
-# Explanation 0
+
+# Explanation 0:
+# 3+2=5
+# 3-2=1
+# 3*2=6
+
+## CODE HERE :
+
+from re import sub
 
 
+a = int(input())
+b = int(input())
+sum = a+b
+subtract = a-b
+multiply = a*b
+print(sum)
+print(subtract)
+print(multiply)
 
-
+# Status: 
