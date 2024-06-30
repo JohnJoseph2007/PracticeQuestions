1d510aafb0059fd53a162335e069a5b936639ff1
John Joseph <47251026+JohnJoseph2007@users.noreply.github.com>
2024-06-30 23:26:11 +0530

    list comprehension done

--- hackerrank/listcomprehension.py
+++ hackerrank/listcomprehension.py
@@ -45,11 +45,13 @@
 
 ## CODE HERE :
 
-x = int(input())
-y = int(input())
-z = int(input())
-n = int(input())
-lista = [x for x in range(1, 11)]
-print(lista)
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    p = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k)!=n]
+    print(p)
 
-# Status: Unfinished (Normal Hackerrank)
+
+# Status: Finished
